a909e2164ea015fa7b31a51513e631451448488c
kebuhvaleriekenchin@gmail.com <kebuhvaleriekenchin@gmail.com>
2026-01-21 06:07:53 +0100

    Initial commit for FarmGlobe project

---
+++ form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="userRgistration.php" method="post">
+    <label for="">first name</label>
+    <input type="text" name="first_name"><br><br>
+    <label for="last_name">last name</label>
+    <input type="text" name="last_name" id="last_name"><br><br>
+    <label for="">phone Number</label>
+    <input type="tel" name="phone_number"><br><br>
+    <label for="">email</label>
+    <input type="text" name="email" id=""><br><br>
+    <label for="">password</label>
+    <input type="password_hash" name="password_hash" id=""><br><br>
+    
+    <input type="password_hash" name="confirm_password" id=""><br>
+    <label for="">Role</label>
+    <input type="text" name="role" list="roles" Id="role">
+    <datalist id="roles">
+      
+      <option value="Farmer" ></option>
+      <option value="Buyer"></option>
+      <option value="Admin"></option>
+      <option value="input supplyer"></option>
+      <option value="logistic operator"></option>
+    </datalist><br><br>
+    <button type="submit">submit</button>
+  </form>
+    
+</body>
+</html>
